10be6bed01409daf28ebc7a924ef08a4417628b6
codeskyblue <codeskyblue@gmail.com>
2014-05-16 23:07:43 +0800

    add more functions

--- models/task.go
+++ models/task.go
@@ -17,7 +17,7 @@ const (
 
 type Repository struct {
 	Id    int64
-	Uri   string
+	Uri   string `xorm:"unique(r)"`
 	Brief string
 }
 
@@ -40,7 +40,8 @@ type DownloadHistory struct {
 type Task struct {
 	Id        int64
 	Rid       int64
-	Branch    string // can also be tag, commit_id
+	Repo      *Repository `xorm:"-"`
+	Branch    string      // can also be tag, commit_id
 	Os        string
 	Arch      string
 	CgoEnable bool
@@ -50,16 +51,56 @@ type Task struct {
 	Updated time.Time `xorm:"updated"`
 }
 
-var ErrTaskNotAvaliable = errors.New("task not avaliable for now")
+var (
+	ErrTaskNotAvaliable    = errors.New("not task ready for build now")
+	ErrTaskNotExists       = errors.New("task not exists")
+	ErrRepositoryNotExists = errors.New("repo not found")
+)
 
 func init() {
 	tables = append(tables, new(Task), new(Repository), new(RepoStatistic), new(DownloadHistory))
+}
+
+func CreateRepository(repoUri string) (*Repository, error) {
+	r := &Repository{Uri: repoUri}
+	if has, err := orm.Get(r); err == nil && has {
+		return r, nil
+	}
+	r.Uri = repoUri
+	r.Brief = "todo, not get"
+	_, err := orm.Insert(r)
+	return r, err
+}
+
+func GetRepositoryById(id int64) (*Repository, error) {
+	r := new(Repository)
+	if has, err := orm.Id(id).Get(r); err == nil && has {
+		return r, nil
+	}
+	return nil, ErrRepositoryNotExists
 }
 
 func CreateTask(task *Task) (*Task, error) {
 	task.Status = ST_READY
 	_, err := orm.Insert(task)
 	return task, err
+}
+
+func GetTaskById(tid int64) (*Task, error) {
+	t := new(Task)
+	has, err := orm.Id(tid).Get(t)
+	if err != nil {
+		return nil, err
+	}
+	if !has {
+		return nil, ErrTaskNotExists
+	}
+	return t, nil
+}
+
+func ResetAllTaskStatus() error {
+	_, err := orm.Where("status != ?", ST_DONE).Update(&Task{Status: ST_READY})
+	return err
 }
 
 func UpdateTaskStatus(tid int64, status string) error {
