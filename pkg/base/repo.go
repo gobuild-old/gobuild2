2504bd60c80441c97b3919e48303e0c26642088f
codeskyblue <codeskyblue@gmail.com>
2014-05-24 17:58:21 +0800

    remove extra space

--- pkg/base/repo.go
+++ pkg/base/repo.go
@@ -3,6 +3,7 @@ package base
 import "strings"
 
 func SanitizedRepoPath(repo string) string {
+	repo = strings.TrimSpace(repo)
 	if strings.HasSuffix(repo, ".git") {
 		repo = repo[:len(repo)-4]
 	}
